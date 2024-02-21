3282b5ab9b42ea9203a80b64839222482f2d5b76
Reva <111722589+rssathe08311@users.noreply.github.com>
2024-02-20 21:14:45 -0500

    more work done on pe 6

--- web-audio-hw/src/audio.js
+++ web-audio-hw/src/audio.js
@@ -16,7 +16,7 @@ const DEFAULTS = Object.freeze({
 let audioData = new Uint8Array(DEFAULTS.numSamples/2);
 
 // **Next are "public" methods - we are going to export all of these at the bottom of this file**
-function setupWebaudio(filePath){
+const  setupWebaudio = filePath => {
 // 1 - The || is because WebAudio has not been standardized across browsers yet
     const AudioContext = window.AudioContext || window.webkitAudioContext;
     audioCtx = new AudioContext(); 
@@ -56,19 +56,19 @@ the amplitude of that frequency.
     gainNode.connect(audioCtx.destination);
 }
 
-function loadSoundFile(filePath){
+const  loadSoundFile = filePath => {
     element.src = filePath;
 }
 
-function playCurrentSound(){
+const playCurrentSound = () => {
     element.play();
 }
 
-function pauseCurrentSound(){
+const pauseCurrentSound = () => {
     element.pause();
 }
 
-function setVolume(value){
+const setVolume = value => {
     value = Number(value);// make sure that it's a Number rather than a String
     gainNode.gain.value = value;
 }
--- web-audio-hw/src/main.js
+++ web-audio-hw/src/main.js
@@ -22,6 +22,7 @@ function init(){
     console.log(audio)
 	let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
 	setupUI(canvasElement);
+  loop();
 }
 
 function setupUI(canvasElement){
@@ -34,7 +35,7 @@ function setupUI(canvasElement){
     utils.goFullscreen(canvasElement);
   };
 
-  // add .onclick event to button
+  //B - add .onclick event to button
   playButton.onclick = e => {
     console.log(`audioCtx.state before = ${audio.audioCtx.state}`);
 
@@ -54,8 +55,61 @@ function setupUI(canvasElement){
         e.target.dataset.playing = "no";//will change the CSS to correlate with current state
     }
   }
-  //on step 8
+  //C - hookup volume slider & label
+  let volumeSlider = document.querySelector("#volumeSlider");
+  let volumeLabel = document.querySelector("#volumeLabel");
+
+  //add .oninput event to slider
+  volumeSlider.oninput = e => {
+    //set the gain
+    audio.setVolume(e.target.value);
+    //update value of label to match value of slider
+    volumeLabel.innerHTML = Math.round((e.target.value/2 * 100));
+  };
+
+  //set value of label to match initial value of slider
+  volumeSlider.dispatchEvent(new Event("input"));
+
+  //D - hookup track <select>
+  let trackSelect = document.querySelector("#trackSelect");
+  //add .onchange event to <select>
+  trackSelect.onchange = e => {
+    audio.loadSoundFile(e.target.value);
+    //pause the current track if it is playing
+    if(playButton.dataset.playing == "yes"){
+      playButton.dispatchEvent(new MouseEvent("click"));
+    }
+  }
 	
 } // end setupUI
+
+function loop(){
+  /* NOTE: This is temporary testing code that we will delete in Part II */
+    requestAnimationFrame(loop);
+    // 1) create a byte array (values of 0-255) to hold the audio data
+    // normally, we do this once when the program starts up, NOT every frame
+    let audioData = new Uint8Array(audio.analyserNode.fftSize/2);
+    
+    // 2) populate the array of audio data *by reference* (i.e. by its address)
+    audio.analyserNode.getByteTimeDomainData(audioData); // waveform data
+    
+    // 3) log out the array and the average loudness (amplitude) of all of the frequency bins
+      console.log(audioData);
+      
+      console.log("-----Audio Stats-----");
+      let totalLoudness =  audioData.reduce((total,num) => total + num);
+      let averageLoudness =  totalLoudness/(audio.analyserNode.fftSize/2);
+      let minLoudness =  Math.min(...audioData); // ooh - the ES6 spread operator is handy!
+      let maxLoudness =  Math.max(...audioData); // ditto!
+      // Now look at loudness in a specific bin
+      // 22050 kHz divided by 128 bins = 172.23 kHz per bin
+      // the 12th element in array represents loudness at 2.067 kHz
+      let loudnessAt2K = audioData[11]; 
+      console.log(`averageLoudness = ${averageLoudness}`);
+      console.log(`minLoudness = ${minLoudness}`);
+      console.log(`maxLoudness = ${maxLoudness}`);
+      console.log(`loudnessAt2K = ${loudnessAt2K}`);
+      console.log("---------------------");
+  }
 
 export {init};
