a79f2e7f051ee82d5bdf5197060b5d05b9ac3404
Reva <111722589+rssathe08311@users.noreply.github.com>
2024-02-21 13:54:57 -0500

    finished pe07

---
+++ sathe-r-pe07/web-audio-hw-2/src/visualizer.js
@@ -0,0 +1,108 @@
+/*
+	The purpose of this file is to take in the analyser node and a <canvas> element: 
+	  - the module will create a drawing context that points at the <canvas> 
+	  - it will store the reference to the analyser node
+	  - in draw(), it will loop through the data in the analyser node
+	  - and then draw something representative on the canvas
+	  - maybe a better name for this file/module would be *visualizer.js* ?
+*/
+
+import * as utils from './utils.js';
+
+let ctx,canvasWidth,canvasHeight,gradient,analyserNode,audioData;
+
+
+const setupCanvas = (canvasElement,analyserNodeRef) => {
+	// create drawing context
+	ctx = canvasElement.getContext("2d");
+	canvasWidth = canvasElement.width;
+	canvasHeight = canvasElement.height;
+	// create a gradient that runs top to bottom
+	gradient = utils.getLinearGradient(ctx,0,0,0,canvasHeight,[{percent:0,color:"yellow"},{percent:.25,color:"orange"},{percent:.5,color:"coral"},{percent:1,color:"magenta"}]);
+	// keep a reference to the analyser node
+	analyserNode = analyserNodeRef;
+	// this is the array where the analyser data will be stored
+	audioData = new Uint8Array(analyserNode.fftSize/2);
+}
+
+const draw = (params={}) => {
+  // 1 - populate the audioData array with the frequency data from the analyserNode
+	// notice these arrays are passed "by reference" 
+	analyserNode.getByteFrequencyData(audioData);
+	// OR
+	//analyserNode.getByteTimeDomainData(audioData); // waveform data
+	
+	// 2 - draw background
+	ctx.save();
+    ctx.fillStyle = "black";
+    ctx.globalAlpha = 0.1;
+    ctx.fillRect(0,0,canvasWidth,canvasHeight);
+    ctx.restore();
+		
+	// 3 - draw gradient
+	if(params.showGradient){
+        ctx.save();
+        ctx.fillStyle = gradient;
+        ctx.globalAlpha = 0.3;
+        ctx.fillRect(0,0,canvasWidth,canvasHeight);
+        ctx.restore();
+
+    }
+	// 4 - draw bars
+	if(params.showBars){
+        let barSpacing = 4;
+        let margin = 5;
+        let screenWidthForBars = canvasWidth - (audioData.length * barSpacing) - margin * 2;
+        let barWidth = screenWidthForBars / audioData.length;
+        let barHeight = 200;
+        let topSpacing = 100;
+
+        ctx.save();
+        ctx.fillStyle = `rgba(255,255,255,0.5)`;
+        ctx.strokeStyle = `rgba(0,0,0,0.5)`;
+        //loop through the data and draw
+        for(let i = 0; i < audioData.length; i++){
+            ctx.fillRect(margin + i * (barWidth + barSpacing), topSpacing + 256-audioData[i], barWidth,barHeight);
+            ctx.strokeRect(margin + i * (barWidth + barSpacing), topSpacing + 256-audioData[i], barWidth,barHeight);
+        }
+        ctx.restore();
+    }
+
+	// 5 - draw circles
+    if(params.showCircles){
+        let maxRadius = canvasHeight/4;
+        ctx.save();
+        ctx.globalAlpha = 0.5;
+        for(let i = 0; i < audioData.length; i++){
+            //blue-ish circles
+            let percent = audioData[i] / 255;
+
+            let circleRadius = percent * maxRadius;
+            ctx.beginPath();
+            ctx.fillStyle = utils.makeColor(0, 100, 200, .5 - percent/5.0);
+            ctx.arc(canvasWidth/2, canvasHeight/2, circleRadius * 1.1, 0, 2 * Math.PI, false);
+            ctx.fill();
+            ctx.closePath();
+
+            //teal-ish circles, bigger, more transparent
+            ctx.beginPath();
+            ctx.fillStyle = utils.makeColor(0, 200, 150, .5 - percent/5.0);
+            ctx.arc(canvasWidth/2, canvasHeight/2, circleRadius * 1.25, 0, 2 * Math.PI, false);
+            ctx.fill();
+            ctx.closePath();
+
+            //blue-ish circles, smaller
+            ctx.save();
+            ctx.beginPath();
+            ctx.fillStyle = utils.makeColor(0, 100, 250, .5 - percent/5.0);
+            ctx.arc(canvasWidth/2, canvasHeight/2, circleRadius * .70, 0, 2 * Math.PI, false);
+            ctx.fill();
+            ctx.closePath();
+            ctx.restore();
+        }
+        ctx.restore();
+    }
+		
+}
+
+export {setupCanvas,draw};
---
+++ sathe-r-pe07/web-audio-hw-3/src/main.js
@@ -0,0 +1,143 @@
+/*
+	main.js is primarily responsible for hooking up the UI to the rest of the application 
+	and setting up the main event loop
+*/
+
+// We will write the functions in this file in the traditional ES5 way
+// In this instance, we feel the code is more readable if written this way
+// If you want to re-write these as ES6 arrow functions, to be consistent with the other files, go ahead!
+
+import * as canvas from './visualizer.js';
+import * as audio from './audio.js';
+import * as utils from './utils.js';
+
+let gradient_checkbox = document.querySelector("#gradientCB");
+let bars_checkbox = document.querySelector("#barsCB");
+let circles_checkbox = document.querySelector("#circlesCB");
+let noise_checkbox = document.querySelector("#noiseCB");
+let invert_checkbox = document.querySelector("#invertCB");
+let emboss_checkbox = document.querySelector("#embossCB");
+
+let drawParams = {
+  showGradient  : true,
+  showBars      : true,
+  showCircles   : true,
+  showNoise     : false,
+  showInvert    : false,
+  showEmboss    : false
+}
+
+// 1 - here we are faking an enumeration
+const DEFAULTS = Object.freeze({
+	sound1  :  "media/New Adventure Theme.mp3"
+});
+
+const init = () => {
+	console.log("init called");
+	console.log(`Testing utils.getRandomColor() import: ${utils.getRandomColor()}`);
+    audio.setupWebaudio(DEFAULTS.sound1);
+    console.log(audio)
+	let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
+
+  gradient_checkbox.checked = drawParams.showGradient;
+  bars_checkbox.checked = drawParams.showBars;
+  circles_checkbox.checked = drawParams.showCircles;
+  noise_checkbox.checked = drawParams.showNoise;
+
+	setupUI(canvasElement);
+  canvas.setupCanvas(canvasElement,audio.analyserNode);
+  loop();
+}
+
+const setupUI = (canvasElement) => {
+  // A - hookup fullscreen button
+  const fsButton = document.querySelector("#fsButton");
+	
+  // add .onclick event to button
+  fsButton.onclick = e => {
+    console.log("goFullscreen() called");
+    utils.goFullscreen(canvasElement);
+  };
+
+  //B - add .onclick event to button
+  playButton.onclick = e => {
+    console.log(`audioCtx.state before = ${audio.audioCtx.state}`);
+
+    //check if context is in suspended state (autoplay policy)
+    if(audio.audioCtx.state == "suspended"){
+        audio.audioCtx.resume();
+    }
+    console.log(`audioCtx.state after = ${audio.audioCtx.state}`);
+    if(e.target.dataset.playing == "no"){
+        //if track is currently paused, play it
+        audio.playCurrentSound();
+        e.target.dataset.playing = "yes";//will change the CSS to correlate with current state
+    }
+    //if track is playing pause it
+    else{
+        audio.pauseCurrentSound();
+        e.target.dataset.playing = "no";//will change the CSS to correlate with current state
+    }
+  }
+  //C - hookup volume slider & label
+  let volumeSlider = document.querySelector("#volumeSlider");
+  let volumeLabel = document.querySelector("#volumeLabel");
+
+  //add .oninput event to slider
+  volumeSlider.oninput = e => {
+    //set the gain
+    audio.setVolume(e.target.value);
+    //update value of label to match value of slider
+    volumeLabel.innerHTML = Math.round((e.target.value/2 * 100));
+  };
+
+  //set value of label to match initial value of slider
+  volumeSlider.dispatchEvent(new Event("input"));
+
+  //D - hookup track <select>
+  let trackSelect = document.querySelector("#trackSelect");
+  //add .onchange event to <select>
+  trackSelect.onchange = e => {
+    audio.loadSoundFile(e.target.value);
+    //pause the current track if it is playing
+    if(playButton.dataset.playing == "yes"){
+      playButton.dispatchEvent(new MouseEvent("click"));
+    }
+  }
+
+  //checkbox event listeners
+  gradient_checkbox.addEventListener('change', function() {
+    drawParams.showGradient = this.checked;
+  });
+
+  bars_checkbox.addEventListener('change', function() {
+    drawParams.showBars = this.checked;
+  });
+
+  circles_checkbox.addEventListener('change', function() {
+      drawParams.showCircles = this.checked;
+  });
+
+  noise_checkbox.addEventListener('change', function() {
+      drawParams.showNoise = this.checked;
+  });
+
+  invert_checkbox.addEventListener('change', function (){
+    drawParams.showInvert = this.checked;
+  });
+
+  emboss_checkbox.addEventListener('change', function() {
+    drawParams.showEmboss = this.checked;
+  })
+  
+	
+} // end setupUI
+
+const loop = () => {
+  //makes it so that the program updates every frame
+    requestAnimationFrame(loop);
+    //function to call on the visualizer module in order to display the animations
+    canvas.draw(drawParams);
+  }
+
+export {init};
